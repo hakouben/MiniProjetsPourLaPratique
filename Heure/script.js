c48f39b5ca035d319151838389843ba7df515250
Benadjaoud Abdelhak <89075329+hakouben@users.noreply.github.com>
2022-02-04 15:56:55 +0100

    projet07

---
+++ Heure/script.js
@@ -0,0 +1,62 @@
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
+const timeEl = document.querySelector('.time');
+const dateEl = document.querySelector('.date');
+const toggle = document.querySelector('.toggle');
+
+const days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+const months = ["Jan", "Fev", "Mar", "Avr", "Mai", "Jun", "Jul", "Aou", "Sep", "Oct", "Nov", "Dec"];
+
+toggle.addEventListener('click', (e) => {
+	const html = document.querySelector('html');
+	if(html.classList.contains('dark')){
+		html.classList.remove('dark');
+		e.target.innerHTML = 'Dark mode';
+	} else {
+		html.classList.add('dark');
+		e.target.innerHTML = 'Light mode';
+	}
+});
+
+function setTime() {
+	const time = new Date();
+	const month = time.getMonth();
+	const day = time.getDay();
+	const date = time.getDate();
+	const hours = time.getHours();
+	const hoursForClock = hours % 12;
+	const minutes = time.getMinutes();
+	const seconds = time.getSeconds();
+	
+	hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
+	minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
+	secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+	
+	timeEl.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+	dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+}
+
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime();
+
+setInterval(setTime, 1000);
+
+//panneau social  
+const floating_btn = document.querySelector(".floating-btn");
+const close_btn = document.querySelector(".close-btn");
+const social_panel_container = document.querySelector(
+  ".social-panel-container"
+);
+
+floating_btn.addEventListener("click", () => {
+  social_panel_container.classList.toggle("visible");
+});
+
+close_btn.addEventListener("click", () => {
+  social_panel_container.classList.remove("visible");
+});
